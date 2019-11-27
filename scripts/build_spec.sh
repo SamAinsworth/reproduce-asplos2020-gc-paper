a0203c0e6697a87e4b1c20d1c1812115c231afbc
Sam Ainsworth <sa614@cam.ac.uk>
2019-11-27 18:28:53 +0000

    correct SPEC shell variable usage

--- scripts/build_spec.sh
+++ scripts/build_spec.sh
@@ -12,8 +12,8 @@ cp spec_confs/aarch64.cfg SPEC/config
 cat <(echo "BASE= $BASE") spec_confs/arm-gem5-dlmalloc.cfg > SPEC/config/arm-gem5-dlmalloc.cfg 
 cd SPEC
 . ./shrc   
-runspec --config=aarch64.cfg --action=build all -I
 runspec --config=arm-gem5-dlmalloc.cfg --action=build all -I 
+runspec --config=aarch64.cfg --action=build all -I
 runspec --config=aarch64.cfg --action=run --size=ref all --iterations=1  -I
 runspec --config=arm-gem5-dlmalloc.cfg --action=run --size=ref all --iterations=1  -I
 cd $BASE/scripts

7e5f066b52a88596e402c5e57988a940feeb587d
Sam Ainsworth <sa614@cam.ac.uk>
2019-11-28 14:49:58 +0000

    Fixed plotting scripts

--- scripts/plot_bitcount.sh
+++ scripts/plot_bitcount.sh
@@ -13,46 +13,40 @@ base=$(grep sim_se $BASE/bitcount/m5out/statsshadowno.txt | awk '{print $2}')
 for n in 1 3 5 7 11
 do
 slow=$(grep sim_se $BASE/bitcount/m5out/statsshadow$n.txt  | awk '{print $2}')
-echo $n $(expr $slow / $base) >> $BASE/plots/shadow.data
-echo "\n" >> $BASE/plots/shadow.data
+echo $n $(echo "scale=3;$slow / $base" | bc -l) >> $BASE/plots/shadow.data
 done
 base=$(grep sim_se $BASE/bitcount/m5out/statscounterno.txt | awk '{print $2}')
 for n in 1 2 4 6 8 12
 do
 slow=$(grep sim_se $BASE/bitcount/m5out/statscounter$n.txt  | awk '{print $2}')
-echo $n $(expr $slow / $base) >> $BASE/plots/counter.data
-echo "\n" >> $BASE/plots/counter.data
+echo $n $(echo "scale=3;$slow / $base" | bc -l) >> $BASE/plots/counter.data
 done
 base=$(grep sim_se $BASE/bitcount/m5out/statsrowhammerno.txt | awk '{print $2}')
 for n in 1 2 4 6 8 12
 do
 slow=$(grep sim_se $BASE/bitcount/m5out/statsrowhammer$n.txt  | awk '{print $2}')
-echo $n $(expr $slow / $base) >> $BASE/plots/rowhammer.data
-echo "\n" >> $BASE/plots/rowhammer.data
+echo $n $(echo "scale=3;$slow / $base" | bc -l) >> $BASE/plots/rowhammer.data
 done
-base=$(grep sim_se $BASE/bitcount/m5out/statsstatscfino.txt | awk '{print $2}')
+base=$(grep sim_se $BASE/bitcount/m5out/statscfino.txt | awk '{print $2}')
 for n in 1 2 4 6 8 12
 do
-slow=$(grep sim_se $BASE/bitcount/m5out/statsstatscfifine$n.txt  | awk '{print $2}')
-echo $n $(expr $slow / $base) >> $BASE/plots/integrityfine.data
-echo "\n" >> $BASE/plots/integrityfine.data
-slow=$(grep sim_se $BASE/bitcount/m5out/statsstatscfi$n.txt  | awk '{print $2}')
-echo $n $(expr $slow / $base) >> $BASE/plots/integrity.data
-echo "\n" >> $BASE/plots/integrity.data
+slow=$(grep sim_se $BASE/bitcount/m5out/statscfifine$n.txt  | awk '{print $2}')
+echo $n $(echo "scale=3;$slow / $base" | bc -l) >> $BASE/plots/integrityfine.data
+slow=$(grep sim_se $BASE/bitcount/m5out/statscfi$n.txt  | awk '{print $2}')
+echo $n $(echo "scale=3;$slow / $base" | bc -l) >> $BASE/plots/integrity.data
 done
-base=$(grep sim_se *$bench/run/run_base_ref_armdlmalloc.0000/m5out/statssanitizerno.txt | awk '{print $2}')
+base=$(grep sim_se $BASE/bitcount/m5out/statssanitizerno.txt | awk '{print $2}')
 for n in 1 2 4 6 8 12 24
 do
-slow=$(grep sim_se *$bench/run/run_base_ref_armdlmalloc.0000/m5out/statssanitizer$n.txt  | awk '{print $2}')
-echo $n $(expr $slow / $base) >> $BASE/plots/sanitizer.data
-echo "\n" >> $BASE/plots/sanitizer.data
+slow=$(grep sim_se $BASE/bitcount/m5out/statssanitizer$n.txt  | awk '{print $2}')
+echo $n $(echo "scale=3;$slow / $base" | bc -l) >> $BASE/plots/sanitizer.data
 done
-echo "\n" >> $BASE/plots/counter.data
-echo "\n" >> $BASE/plots/integrity.data
-echo "\n" >> $BASE/plots/integrityfine.data
-echo "\n" >> $BASE/plots/rowhammer.data
-echo "\n" >> $BASE/plots/sanitizer.data
-echo "\n" >> $BASE/plots/shadow.data
+echo "" >> $BASE/plots/counter.data
+echo "" >> $BASE/plots/integrity.data
+echo "" >> $BASE/plots/integrityfine.data
+echo "" >> $BASE/plots/rowhammer.data
+echo "" >> $BASE/plots/sanitizer.data
+echo "" >> $BASE/plots/shadow.data
 done
 cd $BASE/plots/
 gnuplot counter.gp
--- scripts/plot_spec.sh
+++ scripts/plot_spec.sh
@@ -13,46 +13,46 @@ base=$(grep sim_se $BASE/SPEC/benchspec/CPU2006/*$bench/run/run_base_ref_aarch64
 for n in 1 3 5 7 11
 do
 slow=$(grep sim_se $BASE/SPEC/benchspec/CPU2006/*$bench/run/run_base_ref_aarch64.0000/m5out/statsshadow$n.txt  | awk '{print $2}')
-echo $n $(expr $slow / $base) >> $BASE/plots/shadow.data
-echo "\n" >> $BASE/plots/shadow.data
+echo $n $(echo "scale=3;$slow / $base" | bc -l) >> $BASE/plots/shadow.data
+echo "" >> $BASE/plots/shadow.data
 done
 base=$(grep sim_se $BASE/SPEC/benchspec/CPU2006/*$bench/run/run_base_ref_aarch64.0000/m5out/statscounterno.txt | awk '{print $2}')
 for n in 1 2 4 6 8 12
 do
 slow=$(grep sim_se $BASE/SPEC/benchspec/CPU2006/*$bench/run/run_base_ref_aarch64.0000/m5out/statscounter$n.txt  | awk '{print $2}')
-echo $n $(expr $slow / $base) >> $BASE/plots/counter.data
-echo "\n" >> $BASE/plots/counter.data
+echo $n $(echo "scale=3;$slow / $base" | bc -l) >> $BASE/plots/counter.data
+echo "" >> $BASE/plots/counter.data
 done
 base=$(grep sim_se $BASE/SPEC/benchspec/CPU2006/*$bench/run/run_base_ref_aarch64.0000/m5out/statsrowhammerno.txt | awk '{print $2}')
 for n in 1 2 4 6 8 12
 do
 slow=$(grep sim_se $BASE/SPEC/benchspec/CPU2006/*$bench/run/run_base_ref_aarch64.0000/m5out/statsrowhammer$n.txt  | awk '{print $2}')
-echo $n $(expr $slow / $base) >> $BASE/plots/rowhammer.data
-echo "\n" >> $BASE/plots/rowhammer.data
+echo $n $(echo "scale=3;$slow / $base" | bc -l) >> $BASE/plots/rowhammer.data
+echo "" >> $BASE/plots/rowhammer.data
 done
 base=$(grep sim_se $BASE/SPEC/benchspec/CPU2006/*$bench/run/run_base_ref_aarch64.0000/m5out/statsstatscfino.txt | awk '{print $2}')
 for n in 1 2 4 6 8 12
 do
 slow=$(grep sim_se $BASE/SPEC/benchspec/CPU2006/*$bench/run/run_base_ref_aarch64.0000/m5out/statsstatscfifine$n.txt  | awk '{print $2}')
-echo $n $(expr $slow / $base) >> $BASE/plots/integrityfine.data
-echo "\n" >> $BASE/plots/integrityfine.data
+echo $n $(echo "scale=3;$slow / $base" | bc -l) >> $BASE/plots/integrityfine.data
+echo "" >> $BASE/plots/integrityfine.data
 slow=$(grep sim_se $BASE/SPEC/benchspec/CPU2006/*$bench/run/run_base_ref_aarch64.0000/m5out/statsstatscfi$n.txt  | awk '{print $2}')
-echo $n $(expr $slow / $base) >> $BASE/plots/integrity.data
-echo "\n" >> $BASE/plots/integrity.data
+echo $n $(echo "scale=3;$slow / $base" | bc -l) >> $BASE/plots/integrity.data
+echo "" >> $BASE/plots/integrity.data
 done
 base=$(grep sim_se $BASE/SPEC/benchspec/CPU2006/*$bench/run/run_base_ref_armdlmalloc.0000/m5out/statssanitizerno.txt | awk '{print $2}')
 for n in 1 2 4 6 8 12 24
 do
 slow=$(grep sim_se $BASE/SPEC/benchspec/CPU2006/*$bench/run/run_base_ref_armdlmalloc.0000/m5out/statssanitizer$n.txt  | awk '{print $2}')
-echo $n $(expr $slow / $base) >> $BASE/plots/sanitizer.data
-echo "\n" >> $BASE/plots/sanitizer.data
+echo $n $(echo "scale=3;$slow / $base" | bc -l) >> $BASE/plots/sanitizer.data
+echo "" >> $BASE/plots/sanitizer.data
 done
-echo "\n" >> $BASE/plots/counter.data
-echo "\n" >> $BASE/plots/integrity.data
-echo "\n" >> $BASE/plots/integrityfine.data
-echo "\n" >> $BASE/plots/rowhammer.data
-echo "\n" >> $BASE/plots/sanitizer.data
-echo "\n" >> $BASE/plots/shadow.data
+echo "" >> $BASE/plots/counter.data
+echo "" >> $BASE/plots/integrity.data
+echo "" >> $BASE/plots/integrityfine.data
+echo "" >> $BASE/plots/rowhammer.data
+echo "" >> $BASE/plots/sanitizer.data
+echo "" >> $BASE/plots/shadow.data
 done
 cd $BASE/plots/
 gnuplot counter.gp
